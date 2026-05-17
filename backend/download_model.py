24418514a3bb0a98bf8f82163cffc04b5b9dc766
ephrem823 <efaxalemayehu@gmail.com>
2026-05-18 02:28:26 +0300

    remove token from model download

--- backend/download_model.py
+++ backend/download_model.py
@@ -1,10 +1,7 @@
-import os
 from huggingface_hub import hf_hub_download
 
-token = os.environ.get("HF_TOKEN")
 hf_hub_download(
     repo_id="efaxale/crop-health-model",
     filename="crop_health_model_fixed.h5",
-    local_dir="/app/models",
-    token=token
+    local_dir="/app/models"
 )
